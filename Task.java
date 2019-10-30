87a2a6f8b1862bc34e0ac22248b4492f6eef48c7
JaKuth99 <35343757+JaKuth99@users.noreply.github.com>
2019-10-30 13:47:43 -0400

    Update Task.java

--- Task.java
+++ Task.java
@@ -4,16 +4,18 @@ class {
   private LocalDate dueDate;
   private String description;
   private LocalDateTime created;
-  private LocalDateTime lastUpdate; 
+  private LocalDateTime lastUpdate;
+  private Project project;
   public Task(String name_1, LocalDate dueDate_1, String description_1){
     this.name = name_1;
     this.dueDate = dueDate_1;
     this.description = description_1;
-    
+    this.project = project
   }
   public void setDueDate(LocalDate newDueDate){
     dueDate = newDueDate;
     lastUpdate = now();
+    project.lastUpdate=now(); 
   }
   public LocalDate getDueDate(){
     return dueDate;
@@ -21,6 +23,7 @@ class {
   public void setName(String newName){
     name = newName;
     lastUpdate = now();
+    project.lastUpdate=now(); 
   }
   public String getName(){
     return name;
@@ -28,6 +31,7 @@ class {
   public void setDescription(String newDescription){
     description = newDescription;
     lastUpdate = now();
+    project.lastUpdate=now(); 
   }
   public String getDescription(){
     return description;
