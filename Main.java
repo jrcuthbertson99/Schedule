8f3dc48a8091e10f83d8d0f7b3321179f01c8ef5
JaKuth99 <35343757+JaKuth99@users.noreply.github.com>
2019-11-03 22:31:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+class Main{
+  ArrayList<Project> myProjects;
+  public Main(){
+    this.myProjects = myProjects;
+  }
+  void createNewAccount(String username, String password){
 
+  }
+  void login(String username, String password){
+    createNewAccount(username,password);
+  }
+  void loadProjects(){
+  //have the projects load from a text file into myProjects to be displayed with all the relevant information  
+  }
+}
