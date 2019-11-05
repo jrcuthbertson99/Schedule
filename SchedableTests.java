6195bd07b7ce63e20ad9f936d2387afbc7a40b21
JaKuth99 <35343757+JaKuth99@users.noreply.github.com>
2019-11-04 21:47:49 -0500

    Update SchedableTests.java

--- SchedableTests.java
+++ SchedableTests.java
@@ -1,43 +1,42 @@
-import java.time.LocalDate;  
+import java.time.LocalDate;
 import java.time.Month;
 import java.time.LocalDateTime;
 
 
 public class SchedableTests {
    public static void main(String[] args) {
-   
-    Project project= new Project("Project",LocalDate.of(2019, Month.NOVEMBER, 3) ,"Test"); 
-   
+
+    Project project= new Project("Project",LocalDate.of(2019, Month.NOVEMBER, 3) ,"Test");
+
    project.setDueDate(LocalDate.of(2019, Month.NOVEMBER, 4));
    project.setName("ProjectTest");
    project.setDescription("Testing for project");
-   
+
    Task first = new Task("First Task",LocalDate.of(2019, Month.NOVEMBER, 3),"task description",project );
    first.setDueDate(LocalDate.of(2019, Month.NOVEMBER, 4));
    first.setName("Task1Test");
    first.setDescription("Testing for tasks");
-   
+
    Task second = new Task("Second Task",LocalDate.of(2019, Month.NOVEMBER, 3),"task description",project );
    second.setDueDate(LocalDate.of(2019, Month.NOVEMBER, 4));
    second.setName("Task2Test");
    second.setDescription("Testing for tasks again");
-   
-   
+
+
    project.addTask(first);
-   if (project.steps[0].completion==true){
+   if (project.steps.get(0).getCompletion()==true){
       System.out.print("Not yet! We need to finish!");
    }
    project.addTask(second);
    //need to way to set completion of tasks from project?
    //some sort of printinfo
-   project.steps[1].completion=true;
-   project.addTask(first);
-   if (project.steps[0].completion==false){
+   project.steps.get(1).setCompletion(true);
+   if (project.steps.get(1).getCompletion()==false){
       System.out.print("Wym? We're done here.");
    }
-   
+
    System.out.printf("Project: %s\nDue: %s\nDescription: %s\n\n",project.getName(),project.getDueDate().toString(),project.getDescription());
    System.out.printf("Task: \nDue: %s\nDescripiton: %s\n",first.getName(),first.getDueDate().toString(),first.getDescription());
-   
+
    }
 }
