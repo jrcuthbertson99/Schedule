ffd4054cb4ba5d2f9fb9087de00a68d740197026
JaKuth99 <35343757+JaKuth99@users.noreply.github.com>
2019-11-06 16:55:16 -0500

    fixed to work with Main change

--- Project.java
+++ Project.java
@@ -1,47 +1,77 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.time.Month;
 import java.time.LocalDateTime;
-
-
-public class SchedableTests {
-   public static void main(String[] args) {
-     Main firstRun =new Main();
-     firstRun.createNewAccount("firstGuy","TestRun");
-     firstRun.login("firstGuy", "TestRun");
-    Project project= new Project("Project",LocalDate.of(2019, Month.NOVEMBER, 3) ,"Test");
-    firstRun.myProjects.add(project);
-   project.setDueDate(LocalDate.of(2019, Month.NOVEMBER, 4));
-   project.setName("ProjectTest");
-   project.setDescription("Testing for project");
-
-   Task first = new Task("First Task",LocalDate.of(2019, Month.NOVEMBER, 3),"task description", project);
-   first.setDueDate(LocalDate.of(2019, Month.NOVEMBER, 4));
-   first.setName("Task1Test");
-   first.setDescription("Testing for tasks");
-
-   Task second = new Task("Second Task",LocalDate.of(2019, Month.NOVEMBER, 3),"task description",project);
-   second.setDueDate(LocalDate.of(2019, Month.NOVEMBER, 4));
-   second.setName("Task2Test");
-   second.setDescription("Testing for tasks again");
-
-
-   project.addTask(first);
-   //completion tests
-   if (project.steps.get(0).getCompletion()==true){
-      System.out.print("Not yet! We need to finish!");
-   }
-   project.addTask(second);
-   //need to way to set completion of tasks from project?
-   //some sort of printinfo
-   project.steps.get(1).setCompletion(true);
-   if (project.steps.get(1).getCompletion()==false){
-      System.out.print("Wym? We're done here.");
-   }
-
-   //System.out.printf("Project: %s\nDue: %s\nDescription: %s\n\n",project.getName(),project.getDueDate().toString(),project.getDescription());
-   //System.out.printf("Task: \nDue: %s\nDescripiton: %s\n",first.getName(),first.getDueDate().toString(),first.getDescription());
-   System.out.printf(project.toString());
-   System.out.printf(project.toStringMeta());
-   firstRun.logout();
-   }
+class Project  {
+  private String name;
+  private LocalDate dueDate;
+  private String description;
+  private LocalDateTime created;
+  public LocalDateTime lastUpdate;
+  public ArrayList<Task> steps = new ArrayList<Task>();
+  public Project(String name_1, LocalDate dueDate_1, String description_1){
+    this.name = name_1;
+    this.dueDate = dueDate_1;
+    this.description = description_1;
+    this.created  = LocalDateTime.now();
+    this.lastUpdate = LocalDateTime.now();
+  }
+  public Project() {
+    this.created = LocalDateTime.now();
+  }
+  //public LocalDateTime getCreated()
+  public void setDueDate(LocalDate newDueDate){
+    dueDate = newDueDate;
+    lastUpdate = LocalDateTime.now();
+  }
+  public LocalDate getDueDate(){
+    return dueDate;
+  }
+  public void setName(String newName){
+    name = newName;
+    lastUpdate = LocalDateTime.now();
+  }
+  public String getName(){
+    return name;
+  }
+  public void setDescription(String newDescription){
+    description = newDescription;
+    lastUpdate = LocalDateTime.now();
+  }
+  public String getDescription(){
+    return description;
+  }
+  public void addTask(Task task){
+    steps.add(task);
+    task.setProject(this);
+  }
+  public void dropTask(Task task){
+    steps.remove(task);
+  }
+  //needs a getTask(taskname) to print?
+  public void dropCompleted(ArrayList<Task> steps){
+    for(int i=0;i< steps.size();i++){
+      if (steps.get(i).getCompletion()==true){
+        steps.remove(i);
+      }
+    }
+  }
+  public String toString(){
+    String writtenForm=name+"\n"+dueDate.toString()+"\n"+description;
+    String writtenTasks="";
+    for(int i=0; i<steps.size();i++){
+      writtenTasks+=steps.get(i).toStringMeta()+"\n";
+    }
+    writtenForm+="\n"+writtenTasks;
+    return writtenForm;
+  }
+   public String toStringMeta(){
+    String writtenForm="Project:"+name+"\n Due:"+dueDate.toString()+"\n Description"+description+"\n Created:"+created.toString()+"\n Updated"+lastUpdate.toString();
+    String writtenTasks="";
+    for(int i=0; i<steps.size();i++){
+      writtenTasks+=steps.get(i).toStringMeta()+"\n";
+    }
+    writtenForm+="\n"+writtenTasks;
+    return writtenForm;
+  }
 }
