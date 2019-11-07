2d0fcc52ab2ffbe2e71e670491948d3f812edf30
JaKuth99 <35343757+JaKuth99@users.noreply.github.com>
2019-11-07 13:59:37 -0500

    Uploaded the right class

--- Project.java
+++ Project.java
@@ -2,26 +2,24 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.time.Month;
 import java.time.LocalDateTime;
-class Task {
+class Project  {
   private String name;
   private LocalDate dueDate;
   private String description;
   private LocalDateTime created;
-  private LocalDateTime lastUpdate;
-  private Project project;
-  private boolean complete;
-  public Task(String name_1, LocalDate dueDate_1, String description_1, Project project){
+  public LocalDateTime lastUpdate;
+  public ArrayList<Task> steps = new ArrayList<Task>();
+  public Project(String name_1, LocalDate dueDate_1, String description_1){
     this.name = name_1;
     this.dueDate = dueDate_1;
     this.description = description_1;
-    this.complete = false;
-    this.project = project;
     this.created  = LocalDateTime.now();
+    this.lastUpdate = LocalDateTime.now();
   }
-  //add subtasks array?
-  public Task() {
+  public Project() {
     this.created = LocalDateTime.now();
   }
+  //public LocalDateTime getCreated()
   public void setDueDate(LocalDate newDueDate){
     dueDate = newDueDate;
     lastUpdate = LocalDateTime.now();
@@ -39,27 +37,16 @@ class Task {
   public void setDescription(String newDescription){
     description = newDescription;
     lastUpdate = LocalDateTime.now();
-    project.lastUpdate=LocalDateTime.now();
   }
   public String getDescription(){
     return description;
   }
-  public void markAsCompleted(){
-    complete = true;
-    lastUpdate = LocalDateTime.now();
-    project.lastUpdate=LocalDateTime.now();
+  public void addTask(Task task){
+    steps.add(task);
+    task.setProject(this);
   }
-  public void setProject(Project supertask){
-    project = supertask;
-  }
-  public Project getProject(){
-    return project;
-  }
-  public void setCompletion(boolean completion){
-    complete = completion;
-  }
-  public boolean getCompletion(){
-    return complete;
+  public void dropTask(Task task){
+    steps.remove(task);
   }
   public void setCreated(LocalDateTime createdDate){
     created = createdDate;
@@ -67,12 +54,30 @@ class Task {
   public void setLastUpdate(LocalDateTime updated){
     lastUpdate=updated;
   }
+  //needs a getTask(taskname) to print?
+  public void dropCompleted(ArrayList<Task> steps){
+    for(int i=0;i< steps.size();i++){
+      if (steps.get(i).getCompletion()==true){
+        steps.remove(i);
+      }
+    }
+  }
   public String toString(){
     String writtenForm=name+"\n"+dueDate.toString()+"\n"+description;
+    String writtenTasks="";
+    for(int i=0; i<steps.size();i++){
+      writtenTasks+=steps.get(i).toStringMeta()+"\n";
+    }
+    writtenForm+="\n"+writtenTasks;
     return writtenForm;
   }
-  public String toStringMeta(){
-    String writtenForm="Task\n"+name+"\n"+dueDate.toString()+"\n"+description+"\n"+created.toString()+"\n"+lastUpdate.toString();
+   public String toStringMeta(){
+    String writtenForm="Project"+name+"\n"+dueDate.toString()+"\n"+description+"\n"+created.toString()+"\n"+lastUpdate.toString();
+    String writtenTasks="";
+    for(int i=0; i<steps.size();i++){
+      writtenTasks+=steps.get(i).toStringMeta()+"\n";
+    }
+    writtenForm+="\n"+writtenTasks;
     return writtenForm;
   }
 }
