11418cde3c6fe07ce72cff2019a2ebbdfef1f98c
JaKuth99 <35343757+JaKuth99@users.noreply.github.com>
2019-11-12 12:42:33 -0500

    Added line seperator

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.time.*;
 class Main{
   ArrayList<Project> myProjects;
   private ArrayList<String[]> loginInfo;
-  private String username;
+  public String username;
   private String password;
   public Main(){
     this.myProjects = new ArrayList<Project>();
@@ -13,8 +13,7 @@ class Main{
     this.username = username;
     this.password = password;
     try{
-      File file = new File(System.getProperty("user.dir")+File.separator+"loginInfo");
-      file.createNewFile();
+      File file = new File(System.getProperty("user.dir")+File.separator+"loginInfo.txt");
       Scanner li = new Scanner(file);
       int i=0;
       while(li.hasNext()){
@@ -44,8 +43,8 @@ class Main{
     loginInfo.add(newLoginInfo);
     try{
       File file = new File(System.getProperty("user.dir")+"loginInfo");
-      BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+File.separator+"loginInfo", true));
-      writer.write(newLoginInfo[0]+" "+newLoginInfo[1]+"\n");
+      BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+File.separator+"loginInfo.txt", true));
+      writer.write(newLoginInfo[0]+" "+newLoginInfo[1]+System.lineSeparator());
       writer.close();
     }catch(IOException e){
         System.out.print("Error:"+ e);
@@ -54,52 +53,57 @@ class Main{
   void login(String userName, String password){
     for(int i=0; i<loginInfo.size();i++){
       if(userName.equals(loginInfo.get(i)[0]) && password.equals(loginInfo.get(i)[1])){
-        username =userName;
+        username=userName;
         loadProjects(username);
         return;
+      }
+      else{
+        System.out.print("Sorry, the login info is incorrect");
+        System.exit(0);
       }
    }
  }
  void loadProjects(String user){
    try{
-     File file =new File(System.getProperty("user.dir")+File.separator+username);
-     if(file.exists()){
-       Scanner lp = new Scanner(file);
-       int p =-1;
-       int t = -1;
-       while(lp.hasNextLine()){
-         if(lp.hasNextLine() && lp.nextLine().equals("Project\n")){
-           p++;
-           t=-1;
-           String name = lp.nextLine();
-           String deadlineDate = lp.nextLine();
-           String description = lp.nextLine();
-           String createdDate = lp.nextLine();
-           String lastUpdatedDate = lp.nextLine();
-           LocalDate pulledDueDate = LocalDate.parse(deadlineDate);
-           LocalDateTime pulledCreatedDate = LocalDateTime.parse(createdDate);
-           LocalDateTime pulledUpdateDate = LocalDateTime.parse(deadlineDate);
-           myProjects.add(new Project(name,pulledDueDate,description));
-           myProjects.get(p).setCreated(pulledCreatedDate);
-           myProjects.get(p).setLastUpdate(pulledUpdateDate);
-         }
-         else if (lp.hasNextLine() && lp.nextLine().equals("Task\n")){
-           t++;
-           String name = lp.nextLine();
-           String deadlineDate = lp.nextLine();
-           String description = lp.nextLine();
-           String createdDate = lp.nextLine();
-           String lastUpdatedDate = lp.nextLine();
-           LocalDate pulledDueDate = LocalDate.parse(deadlineDate);
-           LocalDateTime pulledCreatedDate = LocalDateTime.parse(createdDate);
-           LocalDateTime pulledUpdateDate =  LocalDateTime.parse(deadlineDate);
-           myProjects.get(p).steps.add(new Task(name, pulledDueDate, description, myProjects.get(p)));
-           myProjects.get(p).steps.get(t).setCreated(pulledCreatedDate);
-           myProjects.get(p).steps.get(t).setLastUpdate(pulledUpdateDate);
-         }
+     File file =new File(System.getProperty("user.dir")+File.separator+username+".txt");
+     Scanner lp = new Scanner(file);
+     int p =-1;
+     int t = -1;
+     while(lp.hasNextLine()){
+       String title = lp.nextLine();
+       System.out.print(title);
+       if(title.equals("Project"+System.lineSeparator())){
+         p++;
+         t=-1;
+         String name = lp.nextLine();
+         String deadlineDate = lp.nextLine();
+         String description = lp.nextLine();
+         String createdDate = lp.nextLine();
+         String lastUpdatedDate = lp.nextLine();
+         LocalDate pulledDueDate = LocalDate.parse(deadlineDate);
+         LocalDateTime pulledCreatedDate = LocalDateTime.parse(createdDate);
+         LocalDateTime pulledUpdateDate = LocalDateTime.parse(deadlineDate);
+         myProjects.add(new Project(name,pulledDueDate,description));
+         myProjects.get(p).setCreated(pulledCreatedDate);
+         myProjects.get(p).setLastUpdate(pulledUpdateDate);
        }
-       lp.close();
+       else if (title.equals("Task"+System.lineSeparator())){
+         t++;
+         String name = lp.nextLine();
+         System.out.print(name);
+         String deadlineDate = lp.nextLine();
+         String description = lp.nextLine();
+         String createdDate = lp.nextLine();
+         String lastUpdatedDate = lp.nextLine();
+         LocalDate pulledDueDate = LocalDate.parse(deadlineDate);
+         LocalDateTime pulledCreatedDate = LocalDateTime.parse(createdDate);
+         LocalDateTime pulledUpdateDate =  LocalDateTime.parse(deadlineDate);
+         myProjects.get(p).steps.add(new Task(name, pulledDueDate, description, myProjects.get(p)));
+         myProjects.get(p).steps.get(t).setCreated(pulledCreatedDate);
+         myProjects.get(p).steps.get(t).setLastUpdate(pulledUpdateDate);
+       }
      }
+     lp.close();
    }catch(IOException e){
        System.out.print("Error:"+ e);
      }
@@ -108,7 +112,7 @@ class Main{
  void assign(String user, Task task){
    Project assignedP=new Project(task.getProject().getName(),task.getProject().getDueDate(),task.getProject().getDescription());
    try{
-     BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+File.separator+user));
+     BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+File.separator+user+".txt"));
      writer.write(assignedP.toStringMeta());
      writer.close();
    }catch(IOException e){
@@ -117,7 +121,7 @@ class Main{
  }
   void logout(){
     try{
-      File file =new File(System.getProperty("user.dir")+File.separator+username);
+      File file =new File(System.getProperty("user.dir")+File.separator+username+".txt");
       BufferedWriter writer = new BufferedWriter(new FileWriter(file));
      for(int i=0; i<myProjects.size();i++){
       writer.write(myProjects.get(i).toStringMeta());
@@ -126,6 +130,8 @@ class Main{
   }catch(IOException e){
       System.out.print("Error:"+ e);
     }
-    System.exit(0);
+    username=null;
+    return;
+
   }
 }
