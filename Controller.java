933df84859d00a791531cec28991a759298f691a
JaKuth99 <35343757+JaKuth99@users.noreply.github.com>
2019-11-14 10:58:47 -0500

    Psuedocode
    Just trying to help because I don't know what I'm doing

--- Controller.java
+++ Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
 public class Controller {
+/*
+Set up buttons
+
+EventRun(ActionEvent e){
+  if(e.getSource==registerButton){
+    Open fields to input info for createNewAccount 
+  }
+   if(e.getSource==loginButton){
+    Open fields to input info for login 
+  }
+  if(e.getSource==newTask){
+    Open fields to input info for Project.addTask
+  }
+  if(e.getSource==newProject){
+    Open fields to input info for adding new project to myProjects
+  }
+   if(e.getSource==logout){
+    run Main.logout
+  }
+}
+
+*/
 }
