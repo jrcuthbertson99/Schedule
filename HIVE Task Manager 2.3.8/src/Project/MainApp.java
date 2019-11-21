d40771939ae7595a35693ceeb9b89c493f143285
SteveQian <45260137+SteveQian@users.noreply.github.com>
2019-11-21 05:44:01 -0500

    Add files via upload

---
+++ HIVE Task Manager 2.3.8/src/Project/MainApp.java
@@ -0,0 +1,155 @@
+package Project;
+
+
+import java.io.*;
+import java.util.*;
+//import java.text.*;
+//import java.time.*;
+
+import Project.model.Project;
+//import Project.model.Task;
+import Project.model.UserInfo;
+import Project.model.UserInfoManager;
+import Project.view.MenuController;
+import Project.view.ProjectEditDialogController;
+import Project.view.ProjectOverviewController;
+//import Project.view.UserLoginDialogController;
+
+import javafx.application.Application;
+//import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+//import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+//import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+
+public class MainApp extends Application {
+
+	public Stage primaryStage;
+	public BorderPane rootLayout;
+	
+	public ArrayList<String[]> loginInfo;
+	ObservableList<Project> myProjects;
+	public UserInfo userinfo;
+	public final UserInfoManager UserInfoManager = new UserInfoManager();;
+	
+	
+	/**
+	 * The data as an observable list of Project.
+	 */
+//	public ObservableList<Project> projectData = FXCollections.observableArrayList();
+	public MenuController MenuController;
+	public ProjectOverviewController ProjectOverviewController;
+	public ProjectEditDialogController ProjectEditDialogController;
+	
+	/**
+	 * Constructor
+	 */
+	public MainApp() {
+	}
+	
+	void cleanup() {
+	    this.primaryStage.close();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	void startRun(Stage stage) {
+		this.primaryStage = stage;
+		this.primaryStage.setTitle("HIVE Task Manager");
+
+		initRootLayout();
+
+		this.showProjectOverview();
+	}
+	
+	public void restart(Stage stage) {
+	    cleanup();
+	    startRun(stage);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		startRun(primaryStage);
+	}
+	
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+//			rootLayout.getStylesheets().add(getClass().getResource("DarkTheme.css").toExternalForm());
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			scene.getStylesheets().add(getClass().getResource("DarkTheme.css").toExternalForm());
+			primaryStage.setScene(scene);
+			
+			this.MenuController = loader.getController();
+			this.MenuController.setMainApp(this);
+			
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Shows the project overview inside the root layout.
+	 */
+	public void showProjectOverview() {
+		try {
+			// Load project overview.
+
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/ProjectOverview.fxml"));
+			AnchorPane projectOverview = (AnchorPane) loader.load();
+			this.ProjectOverviewController = loader.getController();		// V 
+			this.ProjectOverviewController.setMainApp(this);				// V
+			// Set project overview into the center of root layout.			// switch?
+			rootLayout.setCenter(projectOverview);							// A
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns the main stage.
+	 * @return
+	 */
+	public Stage getPrimaryStage() { return primaryStage; }
+	
+	/**
+	 * Returns the data as an observable list of Projects. 
+	 * @return
+	 */
+	public ObservableList<Project> getProjectData() { return this.UserInfoManager.getUserProjectList(); }
+
+	public ArrayList<String[]> getUserInfo() { return this.loginInfo; }
+	
+	public UserInfoManager getUserInfoManager() { return this.UserInfoManager; }
+
+	public ProjectEditDialogController getProjectEditDialogController() { return this.ProjectEditDialogController; }
+	
+	public void setProjectEditDialogController(ProjectEditDialogController Controller) { this.ProjectEditDialogController = Controller; }
+	
+	public ProjectOverviewController getProjectOverviewController() { return this.ProjectOverviewController; }
+	
+	public MenuController getMenuController() { return this.MenuController; }
+	
+	public void setProjectOverviewController(ProjectOverviewController Controller) { this.ProjectOverviewController = Controller; }
+	
+	
+	
+}
---
+++ HIVE Task Manager 2.3.8/src/Project/view/TaskController.java
@@ -0,0 +1,238 @@
+package Project.view;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+//import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import Project.MainApp;
+import Project.model.Project;
+//import Project.model.Project;
+import Project.model.Task;
+import Project.view.ProjectOverviewController;
+
+
+public class TaskController {
+	@FXML
+	public TableView<Task> TaskTable;
+	@FXML
+	public TableColumn<Task, String> TaskNameColume;
+
+	@FXML
+	public TableColumn<Task, LocalDate> TaskDueDateColume;
+
+	public ProjectOverviewController ProjectOverviewController;
+	public Project project;
+//	
+	// Reference to the main application.
+	public MainApp MainApp;
+	
+	/**
+	 * The constructor.
+	 * The constructor is called before the initialize() method.
+	 */
+	
+	public TaskController() {
+	}
+	
+	public void setMainApp(MainApp MainApp) { 
+		this.MainApp = MainApp;
+	}
+	
+	public MainApp getMainApp() {
+		return this.MainApp;
+	}
+	
+	
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	public void initialize() {
+		// Initialize the project table with the two columns.
+		TaskNameColume.setCellValueFactory(
+				cellData -> cellData.getValue().TaskNameProperty());
+//		TaskDescriptionColume.setCellValueFactory(
+//				cellData -> cellData.getValue().TaskDescriptionProperty());
+		TaskDueDateColume.setCellValueFactory(
+				cellData -> cellData.getValue().TaskDueDateProperty());
+		
+//		TaskTable.getSelectionModel().selectedItemProperty().addListener(
+//	   		 (observable, oldValue, newValue) -> showTasktDetails(newValue));
+	}
+	
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 * 
+	 * @param MainApp
+	 */
+	public void setProject(Project project) {
+//		initialize();
+		this.project = project;
+		ObservableList<Task> TaskList = this.project.getProjectTaskList();
+		// Add observable list data to the table
+		TaskTable.setItems(TaskList);
+
+	}
+	
+	
+	
+	
+	public void refreshTasktList() {
+		TaskTable.setItems(null);
+		TaskTable.setItems(this.project.getProjectTaskList());
+	}
+	
+	public void cleanTaskList() {
+		TaskTable.setItems(null);
+	}
+	
+	
+	public void setProjectController(ProjectOverviewController ProjectOverviewController) {
+		this.ProjectOverviewController = ProjectOverviewController;
+	}
+	
+	
+	/**
+	 * Returns the data as an observable list of Projects. 
+	 * @return
+	 */
+	public ObservableList<Task> getTaskData() {
+		return project.getProjectTaskList();
+	}
+	
+	
+	/**
+	 * Opens a dialog to edit details for the specified project. If the user
+	 * clicks OK, the changes are saved into the provided project object and true
+	 * is returned.
+	 * 
+	 * @param project the project object to be edited
+	 * @return true if the user clicked OK, false otherwise.
+	 */
+	public boolean showTaskEditDialog(Task Task) {
+		try {
+			// Load the fxml file and create a new stage for the popup dialog.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/TaskEditDialog.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+//			page.getStylesheets().add(getClass().getResource("DarkTheme.css").toExternalForm());
+
+			// Create the dialog Stage.
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edit Task");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(this.MainApp.getPrimaryStage());
+			Scene scene = new Scene(page);
+			scene.getStylesheets().add(getClass().getResource("DarkTheme.css").toExternalForm());
+			dialogStage.setScene(scene);
+
+			// Set the project into the controller.
+			TaskEditDialogController controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setTask(Task);
+
+			// Show the dialog and wait until the user closes it
+			dialogStage.showAndWait();
+
+			return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
+	
+	
+	/**
+	 * Called when the user clicks on the delete button.
+	 */
+	@FXML
+	public void handleDeleteTask() {
+		int selectedIndex = TaskTable.getSelectionModel().getSelectedIndex();
+		if (selectedIndex >= 0) {
+			TaskTable.getItems().remove(selectedIndex);
+//			TaskTable.getSelectionModel().selectAboveCell();
+		} else {
+			// Nothing selected.
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(this.MainApp.getPrimaryStage());
+			alert.setTitle("No Selection");
+			alert.setHeaderText("No Task Selected");
+			alert.setContentText("Please select a task in the table.");
+
+			alert.showAndWait();
+		}
+
+	}
+	
+	/**
+	 * Called when the user clicks the new button. Opens a dialog to edit
+	 * details for a new project.
+	 */
+	@FXML
+	public void handleNewTask() {
+		Task tempTask = new Task();
+		boolean okClicked =  this.showTaskEditDialog(tempTask);
+		if (okClicked ) {
+			if (project==null || !this.MainApp.getProjectOverviewController().ProjectInProjectTable(project)) { 
+				Project newProject = new Project("New");
+				newProject.getProjectTaskList().add(tempTask);
+				this.getMainApp().getProjectData().add(newProject);
+				this.MainApp.getProjectOverviewController().tableSelectedLastOne();;
+			} else getTaskData().add(tempTask);
+			this.tableSelectedLastOne();;
+		}
+	}
+
+	/**
+	 * Called when the user clicks the edit button. Opens a dialog to edit
+	 * details for the selected project.
+	 */
+	@FXML
+	public void handleEditTask() {
+		Task selectedTask = TaskTable.getSelectionModel().getSelectedItem();
+		if (selectedTask != null) {
+			this.showTaskEditDialog(selectedTask);
+//			boolean okClicked = this.showTaskEditDialog(selectedTask);
+//			if (okClicked) {
+//				showTaskDetails(selectedTask);
+//			}
+
+		} else {
+			// Nothing selected.
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(this.MainApp.getPrimaryStage());
+			alert.setTitle("No Selection");
+			alert.setHeaderText("No Project Selected");
+			alert.setContentText("Please select a project in the table.");
+
+			alert.showAndWait();
+		}
+	}
+	
+	public void tableSelectedLastOne() {
+		this.TaskTable.getSelectionModel().selectLast();
+	}
+	
+	public void tableSelectedNone() {
+		this.TaskTable.getSelectionModel().selectLast();
+	}
+	
+	public void tableSelectedFirst() {
+		this.TaskTable.getSelectionModel().selectFirst();
+	}
+}
